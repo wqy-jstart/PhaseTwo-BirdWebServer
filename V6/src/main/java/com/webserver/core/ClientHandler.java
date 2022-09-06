0486b523baf5e3df716ea9a58af8e4be0f6f5911
jstart <11465401+jstart@user.noreply.gitee.com>
2022-09-06 18:26:54 +0800

    关于JavaWebServer的代码文件

---
+++ V6/src/main/java/com/webserver/core/ClientHandler.java
@@ -0,0 +1,118 @@
+package com.webserver.core;
+
+import com.webserver.http.HttpServerRequest;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 该线程任务负责处理与某个客户端的一次HTTP交互
+ * 由于HTTP协议要求客户端和服务端采取一问一答的原则，因此这里处理与客户端的一次交互规划
+ * 为三个步骤:
+ * 1:解析请求(将客户端发送过来的请求内容读取到)
+ * 2:处理请求(根据请求内容进行对应的处理)
+ * 3:发送响应(将处理结果回馈给浏览器)
+ * 断开连接
+ */
+public class ClientHandler implements Runnable{
+    private Socket socket;
+    private static File rootDir;
+    private static File staticDir;
+    static {
+        try {
+            //rootDir表示类加载路径:target/classes目录
+            rootDir = new File(
+                    ClientHandler.class.getClassLoader()
+                            .getResource(".").toURI()
+            );
+            //定位static目录(static目录下存放的是所有静态资源)
+            staticDir = new File(rootDir,"static");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    public ClientHandler(Socket socket){
+        this.socket=socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+//            1:解析请求(将客户端发送过来的请求内容读取到)
+            HttpServerRequest request = new HttpServerRequest(socket);//实例化request
+
+//            2:处理请求(根据请求内容进行对应的处理)
+            String path = request.getUri();//将获得的抽象路径赋给path
+
+//            3:发送响应(将处理结果回馈给浏览器)
+            //定位static目录下的HTML文件
+            File file = new File(staticDir,path);//path为项目中的HTML文件
+            System.out.println("文件是否存在："+file.exists());
+            /*
+                测试:给浏览器发送一个响应，包含static目录下的index.html
+                HTTP/1.1 200 OK(CRLF)
+                Content-Type: text/html(CRLF)
+                Content-Length: 2546(CRLF)(CRLF)
+                1011101010101010101......
+             */
+            OutputStream out = socket.getOutputStream();//通过socket获取文件输出流
+            //发送状态行
+            //HTTP/1.1 200 OK(CRLF)
+            println("HTTP/1.1 200 OK");
+
+            //发送响应头
+            //Content-Type: text/html(CRLF)
+            println("Content-Type: text/html");
+
+            //Content-Length: 2546(CRLF)
+            println("Content-Length: "+file.length());
+
+            //单独发送回车+换行表达响应头发送完毕
+            println("");//空字符串
+
+            //发送响应正文
+            //将index.html文件所有数据发送
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[1024*10];//块读
+            int len;//表示一次读取的量
+            while ((len = fis.read(data)) != -1){
+                out.write(data,0,len);//
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //按照HTTP协议要求，处理最后要断开连接
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void println(String line) throws IOException {//被重用的操作
+        OutputStream out = socket.getOutputStream();//通过socket获取输出流
+        out.write(line.getBytes(StandardCharsets.ISO_8859_1));
+        out.write(13);//回车符
+        out.write(10);//换行符
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        File rootDir = new File( //寻找类加载路径
+                ClientHandler.class.getClassLoader()
+                        .getResource(".").toURI()
+        );
+        System.out.println(rootDir);//输出类加载路径
+        //定位static目录(static目录下存放的是所有静态资源)
+        File staticDir = new File(rootDir,"static");
+        System.out.println(staticDir);//输出static目录的全部路径
+        //定位static目录下的index.html
+        File file = new File(staticDir,"index.html");
+        System.out.println("文件是否存在："+file.exists());//true
+    }
+}
---
+++ V6/src/main/java/com/webserver/http/HttpServerRequest.java
@@ -0,0 +1,109 @@
+package com.webserver.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 请求对象,该类的每一个实例用于标识HTTP协议规定的请求内容
+ * 一个请求由三部分构成:
+ * 1:请求行
+ * 2:消息头
+ * 3:消息正文
+ */
+public class HttpServerRequest {
+    private Socket socket;
+    //请求行的相关信息
+    private String method;//请求方式
+    private String uri;//抽象路径
+    private String protocol;//协议版本
+    //消息头的相关信息
+    private Map<String,String> headers = new HashMap<>();//创建一个Map散列表
+
+    public HttpServerRequest(Socket socket) throws IOException {
+        this.socket = socket;
+        //1:解析请求行
+        parseRequestLine();
+        //2:解析消息头
+        parseHeaders();
+        //3:解析消息正文
+        parseContent();
+    }
+    //1：解析请求行
+     private void parseRequestLine () throws IOException {
+        String line = readline();//接收返回的请求行内容,若接收失败就不用解析并给浏览器发信号
+         System.out.println("请求行内容：" + line);
+
+         String[] data = line.split("\\s");//按空格拆分
+         System.out.println("拆分后为：" + Arrays.toString(data));//分了3个元素
+         method = data[0];//获取元素1,赋值给请求方式
+         uri = data[1];//获取元素2,赋值给抽象路径
+         protocol = data[2];//获取元素3,赋值给协议版本
+         //打桩输出
+         System.out.println("method:" + method);
+         System.out.println("uri:" + uri);
+         System.out.println("protocol:" + protocol);
+    }
+    //2：解析消息头
+    private void parseHeaders () throws IOException {
+        //读取消息头
+        while (true) {
+            String line = readline();
+            if (line.isEmpty()) {//如果读取到的是空字符串,说明单独读取到了CRLF
+                break;
+            }
+            System.out.println("消息头：" + line);
+            //将消息头按照":"和空格,拆分为名字和值并作为key,value存入到headers中
+            String[] data = line.split(":\\s");
+            //System.out.println(Arrays.toString(data));//输出拆分后的消息头
+            headers.put(data[0], data[1]);
+        }
+        System.out.println("headers:" + headers);//Map输出的格式为(Key=Value)
+    }
+
+    //3：解析消息正文
+    private void parseContent(){
+
+    }
+
+    private String readline() throws IOException { //重用的代码要抛出去
+        //调用同一个socket对象若干次getInputStream()方法返回的始终是同一条输入流
+        InputStream in = socket.getInputStream();//通过socket获取输入流
+        StringBuilder builder = new StringBuilder();
+        char pre='a',cur='a';//pre表示上次读取的字符，cur表示本次读取的字符
+        int d;//接收read()方法返回读取第几个字节内容(一次读一个字符)
+        while ((d = in.read()) != -1){//当为-1时说明字节读取到末尾
+            cur = (char)d;//本次读到的字符
+            if (pre==13&&cur==10){//若上一个读取的是回车符并且本次读取的是换行符
+                break;//跳出循环
+            }
+            builder.append(cur);//拼接本次读取到的字符
+            pre = cur;//进入下一次循环前将本次读取的字符记作上次读取的字符
+        }
+        return builder.toString().trim();//该返回值类型为String型,变量为line。
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    /**
+     * 根据消息头名字获取对应的值
+     * @param name
+     * @return
+     */
+    public String getHeader(String name) {
+        return headers.get(name);
+    }
+}
