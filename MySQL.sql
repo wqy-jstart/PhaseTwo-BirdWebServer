544ca13877fc74f92414634bec813d37962a915b
wuqingyuan <11465401+jstart@user.noreply.gitee.com>
2022-09-15 12:02:19 +0800

    关于JavaWebServer的代码文件

--- MySQL.sql
+++ MySQL.sql
@@ -247,3 +247,173 @@ INSERT INTO person(name) VALUES ('李四');
 SELECT * FROM person;
 #查看表结构
 DESC person;
+#查看当前数据库创建的所有表
+SHOW TABLES ;
+
+#(19).修改表数据操作:----UPDATE语句
+# UPDATE 表名 SET 字段名1=新值1[,字段2=新值2,...][WHERE 过滤条件]
+
+#通常修改语句要添加WHERE子句,用于添加过滤条件来定位要修改的记录,不添加WHERE子句则是全表所有记录都修改。
+#下面的操作会将person表中每条记录的age字段值都改为55！！！
+UPDATE person SET age=55;
+
+#将李四的年龄改为23岁
+UPDATE person SET age=23 WHERE name='李四';
+
+#WHERE中常用的条件:=,>,>=,<,<=,<>(所有数据库都支持的不等于,！=不是所有数据库都支持)
+#将年龄大于50岁的人的年龄改为25
+UPDATE person SET age=25 WHERE age>50;
+
+#修改字段时,可以将计算表达式的结果进行修改
+#将每个人的年龄涨一岁------这种情况不需要加WHERE条件
+UPDATE person SET age=age+1;
+
+#将年龄为24岁的人改名为 李老四,年龄为55(逗号隔开)
+UPDATE person SET name='李老四',age=55
+WHERE age=24;
+
+SELECT *FROM person;
+
+#删除数据:DELETE语句
+# DELETE FROM 表明 [WHERE 过滤条件]
+# 注意！！！ 不添加WHERE条件则是全表删除！！！---系统会提示
+
+# 删除名字为张三的记录
+DELETE FROM person WHERE name='张三';
+
+UPDATE person SET age=20 WHERE name='张三';
+
+#删除年龄大于25的人
+DELETE FROM person WHERE age>25;
+
+#清空表操作
+DELETE FROM person;
+
+#[4].练习：
+#创建数据库day1db 字符集utf8并使用
+CREATE DATABASE day1db CHARSET=UTF8;
+#使用数据库day1db
+USE day1db;
+#创建t_hero表,有name字段 字符集为utf8
+CREATE TABLE t_hero(
+    name VARCHAR(32)
+)CHARSET=UTF8;
+#修改表名为hero-------RENAME
+RENAME TABLE t_hero TO hero;
+#最后面添加价格字段money
+ALTER TABLE hero ADD money INT(3);
+#最前面添加id字段
+ALTER TABLE hero ADD id INT(3) FIRST;
+#在name后面添加age字段
+ALTER TABLE hero ADD age INT(3) AFTER name;
+#表中添加以下数据: 1,李白,50,6888 2.赵云,30,13888 3.刘备,25,6888
+INSERT INTO hero(id,name,age,money) VALUES (1,'李白',50,6888);
+INSERT INTO hero(id,name,age,money) VALUES (2,'赵云',30,13888);
+INSERT INTO hero(id,name,age,money) VALUES (3,'刘备',25,6888);
+#查看hero表中所有数据
+SELECT * FROM hero;
+#查询价格为6888的英雄名
+SELECT name FROM hero WHERE money=6888;
+#修改刘备的年龄为52岁
+UPDATE hero SET age=52 WHERE name='刘备';
+#修改年龄小于等于50岁的价格为5000
+UPDATE hero SET money=5000 WHERE age<=50;
+#删除价格为5000的信息
+DELETE FROM hero WHERE money=5000;
+#删除表hero
+DELETE FROM hero;
+#删除数据库day1db
+DROP DATABASE day1db;
+#查看有多少的数据库
+SHOW DATABASES;
+# 数据类型
+
+#数字类型
+#整数:INT(m)和BIGINT(m).m表示的是长度 例如:m=5 存数字18 实际存储:00018
+#浮点数:DOUBLE(m,n)。m表示数字长度(整体数字的长度，包含小数)，n表示小数位 DOUBLE(5,3) 99.999
+# INSERT INTO XXX VALUES(12.9984) 实际插入数据时当精度超过可保存范围时，会进行四舍五入
+
+#字符类型
+#CHAR(n):定长字符串。每条记录实际占用的字节空间是定长的，不足的部分补充空字符来满足长度要求
+#        优点:查询速度快  缺点:浪费磁盘空间
+# VARCHAR(n):(变长字符串)。最多存n指定的字节数对应的字符，实际保存是用多少占多少。(推荐)
+#         优点:节省磁盘空间 缺点:查询速度慢
+# TEXT(n):可变长字符串，最大65535
+#
+# 日期时间类型
+# DATE:保存年月日
+# TIME:保存时分秒
+# DATETIME:保存年月日十分秒
+# TIMESTAMP:时间戳，记录UTC时间，从1970-01-01 00:00:00到表示的时间之间经过的毫秒
+USE empdb;
+SELECT * FROM emp;
+
+CREATE TABLE userinfo(
+                         id INT,#整型
+                         name VARCHAR(30),#变长字符
+                         birth DATETIME,#存年月日时分秒
+                         salary DOUBLE(7,2)#浮点型
+);
+
+#字段名可以忽略不写，此时为全列插入，即:VALUES需要指定每一列的值，且顺序，个数，类型必须与表中的字段一致
+INSERT INTO userinfo VALUES (1,'张三','1992-08-02 11:22:05',5000.59);
+
+#插入李四,DATETIME类型在不指定时分秒,默认为00:00:00
+INSERT INTO userinfo VALUES (2,'李四','1999-05-16',6500.23);
+
+# #DATETIME中年月日不能省略否则报错或者默认DATETIME默认为null(MySQL版本不同引起)
+# INSERT INTO userinfo VALUES (3,'王五','06:15:55',1900.788);
+
+#插入DOUBLE类型数字时,如果小数部分超过范围会进行四舍五入
+INSERT INTO userinfo VALUES (3,'王五','1987-04-13 06:15:55',1900.788);
+INSERT INTO userinfo VALUES (4,'赵六','1966-03-24',2000);
+INSERT INTO userinfo VALUES (5,'钱七','1860-01-22',40000);
+
+# #整数超过范围会报错或者补最大值(MySQL版本不同)----不建议这样写
+# INSERT INTO userinfo VALUES (6,'老八','1976-02-18',140000);
+
+#查询表中的所有数据
+SELECT * FROM userinfo;
+
+# 约束
+# 约束是为表中某个字段添加特定的限制条件，只有符合条件的记录才可以保存
+#
+# 主键约束:该字段非空且唯一，用该字段的值唯一表示一条记录
+# 非空约束:该字段的值不允许为空
+# 外键约束:实际开发中几乎不使用外键约束
+
+CREATE TABLE student(
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(30) NOT NULL,#NOT NULL非空约束
+                        age INT(3),
+                        gender CHAR(1) #长度为1,超出按第一个
+);
+DESC student;
+
+#由于name为NOT NULL，因此名字必须指定值，不指定会报错
+INSERT INTO student VALUES (NULL,NULL,55,'m');#名字不能为空
+
+#★id本身的主键约束要求非空且唯一，但是由于有自增，所以当不指定时使用系统生成的自增值(有自增时就不要主动赋值)
+INSERT INTO student VALUES (NULL,'刘桑',55,'m');
+
+#★主键有自增时,可以忽略主键字段的插入
+INSERT INTO student(name,age,gender) VALUES ('克晶',18,'f');
+
+#有非空约束的字段插入数据是不可以忽略(因为忽略的字段默认插入NULL值)
+# INSERT INTO student(age,gender) VALUES (18,'f');
+
+#允许为NULL的字段可以通过显式的指定NULL作为值插入
+INSERT INTO student(name,age,gender) VALUES ('传奇',22,NULL);
+#也可以不指定字段来为该字段隐式的插入NULL值
+INSERT INTO student(name,age) VALUES ('国斌',22);
+
+#查询student表中所有的数据
+SELECT * FROM student;
+
+
+
+
+
+
+
+
---
+++ emp.sql
@@ -0,0 +1,59 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.68-MariaDB : Database - newdb3
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`empdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `empdb`;
+
+/*Table structure for table `dept` */
+
+DROP TABLE IF EXISTS `dept`;
+
+CREATE TABLE `dept` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(14) NOT NULL,
+  `loc` varchar(13) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `DNAME` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `dept` */
+
+insert  into `dept`(`id`,`name`,`loc`) values (1,'神仙','天庭'),(2,'妖怪','盘丝洞'),(3,'普通人','北京'),(4,'赛亚人','外星球');
+
+/*Table structure for table `emp` */
+
+DROP TABLE IF EXISTS `emp`;
+
+CREATE TABLE `emp` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `job` varchar(9) DEFAULT NULL,
+  `manager` int(4) DEFAULT NULL,
+  `hiredate` date DEFAULT NULL,
+  `sal` double(7,2) DEFAULT NULL,
+  `comm` double(7,2) DEFAULT NULL,
+  `dept_id` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `emp` */
+
+insert  into `emp`(`id`,`name`,`job`,`manager`,`hiredate`,`sal`,`comm`,`dept_id`) values (1,'孙悟空','销售',4,'1980-12-17',800.00,NULL,1),(2,'猪八戒','销售',4,'1981-02-20',1600.00,300.00,1),(3,'沙僧','销售',4,'1981-02-22',1250.00,500.00,1),(4,'唐僧','销售经理',8,'1981-04-02',2975.00,NULL,1),(5,'刘备','项目经理',NULL,'1981-09-28',1250.00,1400.00,3),(6,'关羽','程序员',5,'1981-05-01',2850.00,NULL,3),(7,'张飞','程序员',5,'1981-06-09',2450.00,NULL,3),(8,'观音','CEO',NULL,'1981-11-17',5000.00,NULL,1),(9,'白骨精','人事',8,'1981-09-08',1500.00,0.00,2),(10,'蜘蛛精','人事',8,'1981-12-03',950.00,NULL,2),(11,'黑熊怪','市场',8,'1981-12-03',3000.00,NULL,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+set names gbk;
