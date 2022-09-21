bb2a6fe6d7764335ed26a67d548227082bb71afe
jstart <11465401+jstart@user.noreply.gitee.com>
2022-09-21 10:11:47 +0800

    关于使用MySQL的代码文件

--- V20/src/main/java/com/webserver/controller/ArticleController.java
+++ V20/src/main/java/com/webserver/controller/ArticleController.java
@@ -16,11 +16,19 @@ public class ArticleController {
     @RequestMapping("/writeArticle")
     public void write(HttpServletRequest request, HttpServletResponse response){
         System.out.println("开始处理发表文章功能！！！！");
+        //获取表单信息
         String title = request.getParameter("title");
         String author = request.getParameter("author");
         String content = request.getParameter("content");
+        /*
+            有一个验证:如果作者无效，要提示一个页面，该作者不存在!
+
+            1:先根据该文章的作者名去userinfo表中找到该用户，并得到该用户的id(主键值)
+            2:将文章信息插入到article表中，u_id字段插入该用户的主键值
+         */
         //必要的验证工作
-        if (author==null || author.isEmpty()){
+        if (author==null || author.isEmpty() ||
+            content==null || content.isEmpty()){
             response.sendRedirect("/article_info_error.html");
             return;
         }
@@ -32,15 +40,23 @@ public class ArticleController {
                          "WHERE username=?";
             PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1,author);//依据作者来查找userinfo表,并反馈id
-            ResultSet rs = ps.executeQuery();
+            ResultSet rs = ps.executeQuery();//查找,反馈结果
             if (rs.next()) {
+                //获取用户id
                 int id = rs.getInt("id");
                 String sql1 = "INSERT INTO article(title,content,u_id) VALUES (?,?,?)";
                 PreparedStatement ps1 = connection.prepareStatement(sql1);
                 ps1.setString(1,title);
                 ps1.setString(2,content);//将用户输入的内容插入到article表中
                 ps1.setInt(3,id);
-                ps1.executeUpdate();
+                int num = ps1.executeUpdate();//插入,影响内容
+                if (num>0){//影响了表的条数,发表成功！
+                    response.sendRedirect("/write_article_success.html");
+                }else {//没有影响表的条数,发表失败！
+                    response.sendRedirect("/write_article_fail.html");
+                }
+            }else {//没有此作者
+                response.sendRedirect("/have_not_user.html");
             }
         }catch (SQLException throwables){
             throwables.printStackTrace();
--- V20/src/main/java/com/webserver/util/DBUtil.java
+++ V20/src/main/java/com/webserver/util/DBUtil.java
@@ -1,21 +1,36 @@
 package com.webserver.util;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  * 数据库工具类
  */
 public class DBUtil {
+    //Druid时阿里提供的数据库连接池,连接池的主要作用：1.控制连接数量 2.重用连接
+    private static DruidDataSource dds;
     static {//调用该类时静态块里SQL加载路径只被加载一次
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        dds = new DruidDataSource();//实例化连接池对象
+        //设置连接数据库的基本信息：URL,username,password,初始连接数,最大连接数等
+        dds.setUsername("root");//数据库用户名
+        dds.setPassword("root");//数据库密码
+        dds.setUrl("jdbc:mysql://localhost:3306/bbsdb?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai&rewriteBatchedStatements=true");
+        dds.setInitialSize(5);//设置初始线程数
+        dds.setMaxActive(10);//设置最大连接数
     }
+
+    /**
+     * 获取一个数据库连接
+     * @return
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/bbsdb?characterEncoding=utf8&useSSL=false&serverTimezone=Asia/Shanghai&rewriteBatchedStatements=true","root","root");
+        /*
+           连接池返回的连接是在实际数据库驱动提供的连接对象上包装了一层连接池自己提供的连接对象,这个
+            连接对象的close方法并不是真的断开连接，而是将当前连接归还到连接池中。
+         */
+        return dds.getConnection();//直接调用该方法获取连接
     }
 }
